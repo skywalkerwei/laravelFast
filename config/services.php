5fee654577848ddf7507c410157abb818516a3a7
skywalkerwei <skywalkerwei@gmail.com>
2022-04-24 14:34:55 +0800

    third demo

--- config/services.php
+++ config/services.php
@@ -31,4 +31,42 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'easy-sms' => [
+        // HTTP 请求的超时时间（秒）
+        'timeout' => 5.0,
+
+        // 默认发送配置
+        'default' => [
+            // 网关调用策略，默认：顺序调用
+            'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+            // 默认可用的发送网关
+            'gateways' => [
+                 'aliyun'
+            ],
+        ],
+        // 可用的网关配置
+        'gateways' => [
+            'aliyun' => [
+                'access_key_id' => env('EASY_SMS_ALIYUN_KEY_ID'),
+                'access_key_secret' =>  env('EASY_SMS_ALIYUN_API_KEY'),
+                'sign_name' => '',
+            ],
+            'errorlog' => [
+                'file' => '/tmp/easy-sms.log',
+            ],
+            'yunpian' => [
+                'api_key' => env('EASY_SMS_YUNPIAN_API_KEY'),
+            ],
+            //...
+        ],
+    ],
+
+//    app('easy-sms')->send(13188888888, [
+//        'content'  => '您的验证码为: 6379',
+//        'template' => 'SMS_001',
+//        'data' => [
+//            'code' => 6379
+//        ],
+//    ]);
 ];
